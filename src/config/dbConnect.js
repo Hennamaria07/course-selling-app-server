aa1ef32a7c1ea709887e0bf581c4ac224e018a88
Henna Maria <hennamaria2001@gmail.com>
2024-05-12 15:55:15 +0530

    error handled

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,5 +3,8 @@ import mongoose from "mongoose";
 export const dbConnect = () => {
     mongoose.connect(process.env.MONGODB_URI)
     .then((res) => console.log(`DATABASE IS SUCCESSFULLY CONNECTED WITH ${res.connection.host}`))
-    .catch((err) => console.log(`DATABASE CONNECTION ERROR---> ${err.message}`))
+    .catch((err) => {
+        console.log(`DATABASE CONNECTION ERROR---> ${err.message}`);
+        process.exit(1);
+    })
 }
