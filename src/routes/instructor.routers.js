065aed6c90dc9d34231efa54a12c782433edba27
Henna Maria <hennamaria2001@gmail.com>
2024-05-12 14:19:37 +0530

    admin middleware path fiexd

--- src/routes/instructor.routers.js
+++ src/routes/instructor.routers.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticateUser from "../middlewares/instructor.middleware.js";
 import { getAllInstructors, getInstructor, removeInstructor, signin, signup } from "../controllers/instructor.controllers.js";
-import authenticateAdmin from "../middlewares/admin.Middleware.js";
+import authenticateAdmin from "../middlewares/admin.middleware.js";
 
 const router = express.Router();
 
